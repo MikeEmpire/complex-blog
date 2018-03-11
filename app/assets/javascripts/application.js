cf0526716df2b986a68d0bed82b21a4ebcc87670
Michael Olie <aolie94@gmail.com>
2018-03-10 17:39:31 -0800

    added page transition effect from url to url

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,66 @@
 //= require_tree .
 
 document.addEventListener("turbolinks:load", function() {
+	var main = document.querySelector('main');
+	// Bind our listeners on the document object so we can intercept any anchors
+	document.addEventListener('click', function(e) {
+		var el = e.target;
+		// Go up in the nodelist until we find a node with a href
+		while (el && !el.href) {
+			el = el.parentNode;
+		}
+
+		if (el) {
+			e.preventDefault();
+			history.pushState(null, null, el.href);
+			changePage();
+
+			return;
+		}
+	});
+
+	var cache = {};
+
+	// Fetches the html content of a page given its URL
+	function loadPage(url) {
+		return fetch(url, {
+			method: 'GET'
+		}).then(function(response) {
+			return response.text();
+		});
+	}
+
+	function changePage() {
+		// after the URL is already changed
+		var url = window.location.href;
+
+		loadPage(url).then(function(responseText) {
+			var wrapper = document.createElement('div');
+					wrapper.innerHTML = responseText;
+			var oldContent = document.querySelector('.cc');
+			var newContent = wrapper.querySelector('.cc');
+
+			main.appendChild(newContent);
+			animate(oldContent, newContent);
+		});
+	};
+
+	function animate(oldContent, newContent) {
+		oldContent.style.position = 'absolute';
+
+		var fadeOut = oldContent.animate({
+			opacity: [1, 0]
+		}, 1000);
+
+		var fadeIn = newContent.animate({
+			opacity: [0, 1]
+		}, 1000);
+
+		fadeIn.onfinish = function() {
+			oldContent.parentNode.removeChild(oldContent);
+		};
+	}
+
 	(function() {
 		var container = document.querySelector( 'div.container' ),
 			triggerBttn = document.getElementById( 'trigger-overlay' ),
